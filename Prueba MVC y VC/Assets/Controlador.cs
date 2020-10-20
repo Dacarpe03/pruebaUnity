45cac74ef16beb0332ae44caf62b84a8dc2f37fd
Dacarpe03 <45000161+Dacarpe03@users.noreply.github.com>
2020-10-20 18:16:00 +0200

    MVC conseguido

---
+++ Prueba MVC y VC/Assets/Controlador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    public List<FiguraVista> figuras;
+    public FiguraModelo figuraModelo;
+    public FiguraVista figuraPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        figuraModelo = new FiguraModelo();
+        figuraPrefab = Instantiate(figuraPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        figuraModelo.Move();
+        figuraPrefab.Move(figuraModelo.Position);
+    }
+}
---
+++ Prueba MVC y VC/Assets/FiguraModelo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class FiguraModelo
+{
+    private Vector3 position;
+    public Vector3 Position
+    {
+        get
+        {
+            return position;
+        }
+    }
+
+    public FiguraModelo()
+    {
+        position = Vector3.zero;
+    }
+
+    public void Move()
+    {
+        position.x += 7f*Time.deltaTime;
+    }
+    
+}
